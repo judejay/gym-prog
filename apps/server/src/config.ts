c097d5d0ca48f55ad3bae732c73ebb861e241b39
judejay <judejay26@gmail.com>
2024-01-15 19:01:44 +0000

    test 2

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -5,5 +5,5 @@ dotenv.config({ path: __dirname + "/.env" });
 export const CONFIG = {
   nodeEnv: process.env.NODE_ENV ?? "dev",
   port: process.env.PORT ?? 3000,
-  ApiKey: "test",
+  ApiKey: "test2",
 } as const;
