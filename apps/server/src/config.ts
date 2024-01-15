ca494fe2599ffd06461b3eda48f2b82acc3e873f
judejay <judejay26@gmail.com>
2024-01-15 19:07:49 +0000

    git ignore working

--- apps/server/src/config.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: __dirname + "/.env" });
-
-export const CONFIG = {
-  nodeEnv: process.env.NODE_ENV ?? "dev",
-  port: process.env.PORT ?? 3000,
-  ApiKey: "test2",
-} as const;
