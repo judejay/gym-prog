04fde25491f743f246d92b185da29b8ff17ec4cb
judejay <judejay26@gmail.com>
2024-01-16 06:43:42 +0000

    layout

---
+++ apps/frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const Layout: React.FC = () => {
+    const [opened, { toggle }] = useDisclosure();
+
+    return (
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !opened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                <div>Logo</div>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+            <AppShell.Main>Main</AppShell.Main>
+        </AppShell>
+    );
+};
+
+export default Layout;
