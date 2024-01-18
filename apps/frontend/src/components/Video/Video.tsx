5c99e62a22e963bc311d39fd8d865cbdafec4395
judejay <judejay26@gmail.com>
2024-01-18 17:17:49 +0000

    video aspect ratio

--- apps/frontend/src/components/Video/Video.tsx
+++ apps/frontend/src/components/Video/Video.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { useMyContext } from '../../hooks/useContext';
+import { AspectRatio } from '@mantine/core';
 
-type Props = {
-}
 
-const Video: React.FC<Props> = () => {
+
+const Video: React.FC = () => {
     const { selectedExercise } = useMyContext();
-
     return (
-        <iframe width="420" height="315"
-            src={selectedExercise?.videoUrl}>
-        </iframe>
 
+        <AspectRatio ratio={16 / 9}>
+            <iframe
+                src={selectedExercise?.videoUrl}
+                title="YouTube video player"
+                style={{ border: 0 }}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            />
+        </AspectRatio>
     )
 }
 
