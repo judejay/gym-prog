cd5a2e4ac34c8004d5848be4917c41c9ca52cfaf
judejay <judejay26@gmail.com>
2024-01-18 17:25:27 +0000

    style: background tabs container

--- apps/frontend/src/components/TabsContainer/TabsContainer.tsx
+++ apps/frontend/src/components/TabsContainer/TabsContainer.tsx
@@ -1,37 +1,39 @@
-import { Tabs, rem } from '@mantine/core';
+import { Container, Tabs, rem } from '@mantine/core';
 import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react';
 import Detail from '../Instructions/Instructions';
 import Video from '../Video/Video';
 import ProgramSettings from '../ProgramSettings/ProgramSettings';
-
+import './TabsContainer.css';
 function TabsContainer() {
   const iconStyle = { width: rem(12), height: rem(12) };
 
   return (
-    <Tabs color="rgba(148, 74, 74, 1)" radius="xs" defaultValue="messages">
-      <Tabs.List><Tabs.Tab value="messages" leftSection={<IconMessageCircle style={iconStyle} />}>
-        Instructions
-      </Tabs.Tab>
-        <Tabs.Tab value="gallery" leftSection={<IconPhoto style={iconStyle} />}>
-          Video
+    <div className='tabs-container'>
+      <Tabs color="rgba(148, 74, 74, 1)" radius="xs" defaultValue="messages">
+        <Tabs.List><Tabs.Tab value="messages" leftSection={<IconMessageCircle style={iconStyle} />}>
+          Instructions
         </Tabs.Tab>
+          <Tabs.Tab value="gallery" leftSection={<IconPhoto style={iconStyle} />}>
+            Video
+          </Tabs.Tab>
 
-        <Tabs.Tab value="settings" leftSection={<IconSettings style={iconStyle} />}>
-          Program Settings
-        </Tabs.Tab>
-      </Tabs.List>
+          <Tabs.Tab value="settings" leftSection={<IconSettings style={iconStyle} />}>
+            Program Settings
+          </Tabs.Tab>
+        </Tabs.List>
 
-      <Tabs.Panel value="gallery">
-        <Video />
-      </Tabs.Panel>
+        <Tabs.Panel value="gallery">
+          <Video />
+        </Tabs.Panel>
 
-      <Tabs.Panel value="messages">
-        <Detail />      </Tabs.Panel>
+        <Tabs.Panel value="messages">
+          <Detail />      </Tabs.Panel>
 
-      <Tabs.Panel value="settings">
-        <ProgramSettings />
-      </Tabs.Panel>
-    </Tabs>
+        <Tabs.Panel value="settings">
+          <ProgramSettings />
+        </Tabs.Panel>
+      </Tabs>
+    </div>
   );
 }
 
