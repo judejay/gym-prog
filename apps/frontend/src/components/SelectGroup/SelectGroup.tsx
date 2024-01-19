3cb13067d452d0979085b5471cd03cace6acf767
judejay <judejay26@gmail.com>
2024-01-19 11:28:08 +0000

    Feat: Unique equipment

--- apps/frontend/src/components/SelectGroup/SelectGroup.tsx
+++ apps/frontend/src/components/SelectGroup/SelectGroup.tsx
@@ -1,35 +1,29 @@
 import { Button, ComboboxItem, OptionsFilter, Popover, Select } from '@mantine/core';
 import './SelectGroup.css';
 import { useMyContext } from '../../hooks/useContext';
+import { useState } from 'react';
 function SelectGroup() {
     const { exerciseData } = useMyContext();
-
-    const optionsFilter: OptionsFilter = ({ options, search }) => {
-        const splittedSearch = search.toLowerCase().trim().split(' ');
-        return (options as ComboboxItem[]).filter((option) => {
-            const words = option.label.toLowerCase().trim().split(' ');
-            return splittedSearch.every((searchWord) => words.some((word) => word.includes(searchWord)));
-        });
-    };
+    const [value, setValue] = useState<ComboboxItem | null>(null);
+    const filteredEquipment = exerciseData.map((exercise) => exercise.equipment);
+    const uniqueEquipment = [...new Set(filteredEquipment)];
+    const filteredExerciseData = exerciseData.filter(exercise => exercise.equipment.toLocaleLowerCase().includes(value?.value.toLocaleLowerCase() || ''));
+    console.log("filteredExerciseData", filteredExerciseData);
 
     return (
-        <Popover width={300} position="bottom" withArrow shadow="md">
-            <Popover.Target>
-                <Button>Filter by Equipment</Button>
-            </Popover.Target>
-            <Popover.Dropdown>
-                <Select className='select'
-                    filter={optionsFilter}
-                    checkIconPosition="right"
-                    mt="md"
-                    allowDeselect
-                    clearable
-                    label="Equipment"
-                    placeholder="Pick value"
-                    data={['Dumbbells', 'Barbell', 'Atlas Stone', 'Machine', 'Body Only', 'Other']}
-                    comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
-                /></Popover.Dropdown>
-        </Popover>
+
+        <Select className='select'
+            value={value ? value.value : null}
+            onChange={(_value, option) => setValue(option)}
+            checkIconPosition="right"
+            mt="md"
+            allowDeselect
+            clearable
+            label="Equipment"
+            placeholder="Pick value"
+            data={uniqueEquipment}
+            comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+        />
 
     );
 }
