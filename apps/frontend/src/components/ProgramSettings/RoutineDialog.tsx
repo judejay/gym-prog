917ec8164f455e0fa9b9eca364bfb10b1e8a7112
judejay <judejay26@gmail.com>
2024-01-30 13:13:12 +0000

    feat: exercises added to routine

--- apps/frontend/src/components/ProgramSettings/RoutineDialog.tsx
+++ apps/frontend/src/components/ProgramSettings/RoutineDialog.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { useMyContext } from '../../hooks/useMyContext';
+import { useRoutineReducer } from '../../ducks/routine_reducer';
 
 type RoutineDialogProps = {
 }
 
 const RoutineDialog: React.FC<RoutineDialogProps> = () => {
+    const { routine, dispatch } = useRoutineReducer();
+
     const { selectedExercise } = useMyContext();
     console.log("selected", selectedExercise);
+    function onHandleAddExercise(event: React.MouseEvent<HTMLElement>): void {
+        event.preventDefault();
+        if (selectedExercise) {
+            dispatch({ type: 'ADD_EXERCISE', payload: selectedExercise });
+        } else {
+            console.error('No exercise selected');
+        }
+    }
+
     return (
         <div>
-            <button>Add to Routine  </button>
+            <button onClick={onHandleAddExercise}>Add to Routine  </button>
+            <p>Current Routine:{routine.exercises.map(ex => <>{ex.name}</>)}</p>
         </div>
     )
 }
--- apps/frontend/src/components/ProgramSettings/RoutineSettings.tsx
+++ apps/frontend/src/components/ProgramSettings/RoutineSettings.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import CardHeading from '../CardHeading/CardHeading';
 import { Paper } from '@mantine/core';
 //import './RoutineSettings.css'
-import { useRoutineReducer } from '../../ducks/routine_reducer';
 import RoutineDialog from './RoutineDialog';
 
 type Props = {}
 
 const RoutineSettings: React.FC<Props> = () => {
-    const { routine } = useRoutineReducer();
-    console.log('routine', routine)
     return (
         <Paper withBorder radius="md" className="card">
             <CardHeading />
