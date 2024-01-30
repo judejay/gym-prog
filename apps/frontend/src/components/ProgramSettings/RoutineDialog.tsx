a104ec5cc7210302384101fd7253e0c9c9913f69
judejay <judejay26@gmail.com>
2024-01-30 16:26:38 +0000

    Feat: routine settings

--- apps/frontend/src/components/ProgramSettings/RoutineDialog.tsx
+++ apps/frontend/src/components/ProgramSettings/RoutineDialog.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useMyContext } from '../../hooks/useMyContext';
 import { useRoutineReducer } from '../../ducks/routine_reducer';
-
+import { Table } from '@mantine/core';
 type RoutineDialogProps = {
 }
+
+
 
 const RoutineDialog: React.FC<RoutineDialogProps> = () => {
     const { routine, dispatch } = useRoutineReducer();
@@ -19,10 +21,31 @@ const RoutineDialog: React.FC<RoutineDialogProps> = () => {
         }
     }
 
+    const rows = routine.exercises.map((element) => (
+        <Table.Tr key={element.name}>
+            <Table.Td>{element.name}</Table.Td>
+            <Table.Td>{element.muscle}</Table.Td>
+            <Table.Td>{element.difficulty}</Table.Td>
+            <Table.Td>{element.equipment}</Table.Td>
+        </Table.Tr>
+    ));
+
     return (
         <div>
             <button onClick={onHandleAddExercise}>Add to Routine  </button>
-            <p>Current Routine:{routine.exercises.map(ex => <>{ex.name}</>)}</p>
+            <Table stickyHeader stickyHeaderOffset={60}>
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Exercise name</Table.Th>
+
+                        <Table.Th>Muscle</Table.Th>
+                        <Table.Th>Difficulty</Table.Th>
+                        <Table.Th>Equipment</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>{rows}</Table.Tbody>
+                <Table.Caption>Scroll page to see sticky thead</Table.Caption>
+            </Table>
         </div>
     )
 }
