c06415a19b82c1f5f52fa5e78084b081d55653bc
judejay <judejay26@gmail.com>
2024-01-30 19:04:24 +0000

    Feat: Routine Tab

---
+++ apps/frontend/src/components/Routine/Routine.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Routine: React.FC = () => {
+    return (
+        <div>
+            <h1>Routine</h1>
+        </div>
+    );
+};
+
+export default Routine;
--- apps/frontend/src/components/TabsContainer/TabsContainer.tsx
+++ apps/frontend/src/components/TabsContainer/TabsContainer.tsx
@@ -2,34 +2,43 @@ import { Tabs, rem } from '@mantine/core';
 import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react';
 import Detail from '../Instructions/Instructions';
 import Video from '../Video/Video';
-import RoutineSettings from '../ProgramSettings/RoutineSettings';
+import RoutineSettings from '../RoutineSettings/RoutineSettings';
+import Routine from '../Routine/Routine';
 
 function TabsContainer() {
   const iconStyle = { width: rem(12), height: rem(12) };
 
   return (
-    <Tabs mt={20} color="rgba(148, 74, 74, 1)" radius="xs" defaultValue="messages">
-      <Tabs.List><Tabs.Tab value="messages" leftSection={<IconMessageCircle style={iconStyle} />}>
-        Instructions
-      </Tabs.Tab>
-        <Tabs.Tab value="gallery" leftSection={<IconPhoto style={iconStyle} />}>
+    <Tabs mt={20} color="rgba(148, 74, 74, 1)" radius="xs" defaultValue="instructions">
+      <Tabs.List>
+        <Tabs.Tab value="instructions" leftSection={<IconMessageCircle style={iconStyle} />}>
+          Instructions
+        </Tabs.Tab>
+        <Tabs.Tab value="video" leftSection={<IconPhoto style={iconStyle} />}>
           Video
         </Tabs.Tab>
 
         <Tabs.Tab value="settings" leftSection={<IconSettings style={iconStyle} />}>
           Routine Settings
         </Tabs.Tab>
+        <Tabs.Tab value="routine" leftSection={<IconSettings style={iconStyle} />}>
+          Routine
+        </Tabs.Tab>
       </Tabs.List>
 
-      <Tabs.Panel value="gallery">
+      <Tabs.Panel value="video">
         <Video />
       </Tabs.Panel>
 
-      <Tabs.Panel value="messages">
-        <Detail />      </Tabs.Panel>
+      <Tabs.Panel value="instructions">
+        <Detail />
+      </Tabs.Panel>
 
       <Tabs.Panel value="settings">
         <RoutineSettings />
+      </Tabs.Panel>
+      <Tabs.Panel value="routine">
+        <Routine />
       </Tabs.Panel>
     </Tabs>
   );
