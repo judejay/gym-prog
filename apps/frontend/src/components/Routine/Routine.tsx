048aeb50a09f8fe7854d91dbd9b77f9000b0688c
judejay <judejay26@gmail.com>
2024-02-09 15:49:53 +0000

    usability issue : no auto play

--- apps/frontend/src/components/Routine/Routine.tsx
+++ apps/frontend/src/components/Routine/Routine.tsx
@@ -1,16 +1,14 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { RoutineContext } from "../../state/Routine_Context";
 import CarouselCard from "../Carousel/CarouselCard";
 import { Carousel } from "@mantine/carousel";
 import { useMediaQuery } from "@mantine/hooks";
 import { useMantineTheme, rem } from "@mantine/core";
-import Autoplay from "embla-carousel-autoplay";
 
 const Routine: React.FC = () => {
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
   const { state } = useContext(RoutineContext);
-  const autoplay = useRef(Autoplay({ delay: 2000 }));
   const slides = state.exercises.map((item) => (
     <Carousel.Slide key={item.exerciseId}>
       <CarouselCard {...item} />
@@ -24,7 +22,6 @@ const Routine: React.FC = () => {
       slidesToScroll={mobile ? 1 : 2}
       height="max"
       withIndicators
-      plugins={[autoplay.current]}
     >
       {slides}
     </Carousel>
